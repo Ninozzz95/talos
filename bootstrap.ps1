8a2ec06ee6fb22701cc54dd6ff3e1656c88243e5
Antonio Rizzo <ninozz142@gmail.com>
2026-09-22 18:32:48 +0200

    test(cli): align input fast-path RED bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Snapshot = '083bc8e5b1474cb101f3f17b26a2d84d94565d93'
+$Snapshot = '0df89c1b605f7a4a167f215a3f038191283776c6'
 $NodeVersion = '24.18.0'
 $NodeArchiveName = 'node-v24.18.0-win-x64.zip'
 $NodeUrl = "https://nodejs.org/dist/v$NodeVersion/$NodeArchiveName"
