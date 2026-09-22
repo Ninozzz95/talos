bf0d919210dbdaaf8ff947fa2355496e2b7ad800
Antonio Rizzo <ninozz142@gmail.com>
2026-09-22 19:27:57 +0200

    test(cli): align word-selection RED bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Snapshot = '9e20c5e9d21f27fd705a5f45b26ba2cd4be2c218'
+$Snapshot = 'b642f04955bf8ac7cfc2ddfbccca756d4e4be102'
 $NodeVersion = '24.18.0'
 $NodeArchiveName = 'node-v24.18.0-win-x64.zip'
 $NodeUrl = "https://nodejs.org/dist/v$NodeVersion/$NodeArchiveName"
