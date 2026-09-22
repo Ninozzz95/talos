4d3997f0767d757ada4b87a034b8f71529c90cb7
Antonio Rizzo <ninozz142@gmail.com>
2026-09-22 21:43:23 +0200

    test(cli): align bootstrap with paste IME wiring checkpoint

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Snapshot = '71e34c8b96b11e25333878c76d043777f0082ea2'
+$Snapshot = '9a0918a3d85895a854a0666cdfbf6132bf2fe8a8'
 $NodeVersion = '24.18.0'
 $NodeArchiveName = 'node-v24.18.0-win-x64.zip'
 $NodeUrl = "https://nodejs.org/dist/v$NodeVersion/$NodeArchiveName"
