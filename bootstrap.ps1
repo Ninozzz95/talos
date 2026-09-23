704a3c5fff8371714aa613b79e004440990382c3
Antonio Rizzo <ninozz142@gmail.com>
2026-09-23 08:54:34 +0200

    test(cli): align bootstrap to final proof-alignment checkpoint

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Snapshot = '38b870b95c3e050ca45705839bed067d96b04cc1'
+$Snapshot = '500158ec3745ff8969fae8caa28c4e779d2ab8a5'
 $NodeVersion = '24.18.0'
 $NodeArchiveName = 'node-v24.18.0-win-x64.zip'
 $NodeUrl = "https://nodejs.org/dist/v$NodeVersion/$NodeArchiveName"
